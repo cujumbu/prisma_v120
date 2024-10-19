2f2ec1cb7844db05aeaf5742e8ba021a71c7ac94
cujumbu <nethandelskompagniet@gmail.com>
2024-10-19 18:59:36 +0200

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -118,6 +118,7 @@ const Login: React.FC = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            autoComplete={isCreatingAdmin ? "new-password" : "current-password"}
           />
         </div>
         <button
