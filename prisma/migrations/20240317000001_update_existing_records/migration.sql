bdfa3d55ee292d89abd0cbd7a22f3f7e5d9c0b4b
cujumbu <nethandelskompagniet@gmail.com>
2024-10-20 12:09:53 +0200

    Delete prisma/migrations/20240317000001_update_existing_records directory

--- prisma/migrations/20240317000001_update_existing_records/migration.sql
+++
@@ -1,9 +0,0 @@
--- Update existing Claim records
-UPDATE "Claim"
-SET "userId" = (SELECT "id" FROM "User" WHERE "isAdmin" = true LIMIT 1)
-WHERE "userId" IS NULL;
-
--- Update existing Return records
-UPDATE "Return"
-SET "userId" = (SELECT "id" FROM "User" WHERE "isAdmin" = true LIMIT 1)
-WHERE "userId" IS NULL;
